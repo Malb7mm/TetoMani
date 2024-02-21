68e65a95df8e60d984e1c3adc52a65d719ede7cb
Malb7mm <ykmeak9g@gmail.com>
2024-02-22 04:10:41 +0900

    バージョン表記追加

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <div class="sidebar">
-    <div class="title-text">TetoMani</div>
+    <div class="title-text">TetoMani<span style="font-size: 14px"> v0.1</span></div>
     <div class="dot"></div>
 
     <nav class="nav-top">
