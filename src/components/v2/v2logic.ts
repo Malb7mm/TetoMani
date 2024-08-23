8c36c25032cf3ee0c9d1b9fa556ad45fba021999
Malb7mm <ykmeak9g@gmail.com>
2024-08-23 23:43:54 +0900

    シェイプのスポーン位置もとめるメソッド追加

--- src/components/v2/v2logic.ts
+++ src/components/v2/v2logic.ts
@@ -1,3 +1,6 @@
+/**
+ * Yは0が下！
+ */
 class XY {
   x: number = 0;
   y: number = 0;
@@ -17,15 +20,17 @@ enum Direction {
 
 class Shape {
   // XY[向き][i番目のブロック]の座標
-  shapes: XY[][];
+  shapes: XY[][]
 
-  constructor(shapeCoords: number[][], isEven: boolean = false) {
-    this.shapes = Array(4).fill(Array(4));
+  constructor(shapeCoords: number[][], width: number | undefined = undefined) {
+    if (width === undefined)
+      width = Math.max(...shapeCoords.flat());
+    this.shapes = Array(width).fill(Array(width));
 
     for (let i = 0; i < 4; i++) {
       let x = shapeCoords[i][0];
       let y = shapeCoords[i][1];
-      let maxIndex = isEven ? 3 : 2;
+      let maxIndex = width - 1;
       
       this.shapes[0][i] = new XY(x, y);
       for (let j = 1; j < 4; j++) {
@@ -38,4 +43,22 @@ class Shape {
   get(rotation: number): XY[] {
     return this.shapes[rotation];
   }
+
+  spawnsAt(lowerEdge: number): XY {
+    // 正面のシェイプ
+    let xCoords = this.shapes[0].map(e => e.x);
+    let yCoords = this.shapes[0].map(e => e.y);
+
+    // Xは幅が偶数なら中央、奇数なら中央左寄り
+    // 例：幅4はX=3、幅3もX=3、幅2はX=4
+    let width = Math.max(...xCoords) - Math.min(...xCoords);
+    let x = 5 - Math.ceil(width / 2);
+
+    // Yはブロックの下端が常に同じ高さにつくように
+    let y = lowerEdge - Math.min(...yCoords);
+
+    return new XY(x, y);
+  }
 }
+
+export {Shape};
