72f85683cfaa1778e330c2b7781e4692a7ef5f34
Malb7mm <ykmeak9g@gmail.com>
2024-08-25 06:26:41 +0900

    piecebagの実装変更

--- src/components/v2/v2logic.ts
+++ src/components/v2/v2logic.ts
@@ -1,3 +1,5 @@
+/// <reference lib="es2021" />
+
 type BagSet = string[];
 type RotationType = "cw" | "ccw" | "180";
 type Direction = 0 | 1 | 2 | 3;
@@ -26,7 +28,7 @@ class Shape {
   constructor(shapeCoords: number[][], width: number | undefined = undefined) {
     if (width === undefined)
       width = Math.max(...shapeCoords.flat());
-    this.shapes = Array(width).fill(Array(width));
+    this.shapes = Array(4).fill(Array(width));
 
     for (let i = 0; i < shapeCoords.length; i++) {
       let x = shapeCoords[i][0];
@@ -62,38 +64,177 @@ class Shape {
   }
 }
 
-class PieceBag {
-  pieces: string[];
-  bagSet: BagSet;
+type BagElementType = "single" | "grouped" | "random-grouped";
 
-  constructor(bagSet: string[]) {
-    this.pieces = [];
-    this.bagSet = bagSet;
-    this.restock();
+class PieceBagElement {
+  value: PieceBagElement[] | string;
+  elementType: BagElementType;
+  doMidwayTerminate: boolean;
+  terminate: number;
+  queue: string[] = [];
+
+  constructor(elementType: "single", value: string);
+  constructor(elementType: "grouped");
+  constructor(elementType: "random-grouped", terminate: number);
+  constructor(elementType: BagElementType, arg2?: any) {
+    this.elementType = elementType;
+    this.value = (typeof arg2 === "string") ? arg2 : [];
+    this.terminate = (typeof arg2 === "number") ? arg2 : 0;
+    this.doMidwayTerminate = typeof arg2 === "number";
   }
 
-  pickNext(): string {
-    if (this.pieces.length === 0)
-      this.restock();
-    return this.pieces.shift()!;
-  }
-
-  getNexts(count: number): string[] {
-    while (this.pieces.length < count)
-      this.restock();
-    return this.pieces.slice(0, count);
-  }
-
-  private restock() {
-    let newBag = [...this.bagSet];
-    for (let i = newBag.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var tmp = newBag[i];
-      newBag[i] = newBag[j];
-      newBag[j] = tmp;
+  shallowCopy(): PieceBagElement {
+    if (typeof this.value === "string") {
+      return new PieceBagElement("single", this.value);
     }
-    this.pieces = this.pieces.concat(newBag);
+    else if (this.value instanceof Array) {
+      let e: PieceBagElement;
+      if (this.elementType === "grouped")
+        e = new PieceBagElement("grouped");
+      else
+        e = new PieceBagElement("random-grouped", this.terminate);
+      e.add(this.value);
+      return e;
+    }
+    else { // 分岐することはない（コンパイラ用）
+      throw new Error("");
+    }
   }
+
+  add(elements: PieceBagElement[]) {
+    if (this.value instanceof Array)
+      for (let e of elements)
+        this.value.push(e);
+      this.terminate
+  }
+
+  generateQueue(): string[] {
+    let elements = this.value;
+    if (this.elementType === "random-grouped" && elements instanceof Array) {
+      for (let i = elements.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = elements[i];
+        elements[i] = elements[j];
+        elements[j] = tmp;
+      }
+      elements = elements.slice(0, this.terminate);
+    }
+
+    if (typeof elements === "string") {
+      this.queue = [elements];
+    }
+    else {
+      let queue: string[] = [];
+      for (let element of elements)
+        queue.push(...element.generateQueue());
+      this.queue = queue;
+    }
+
+    return this.queue;
+  }
+
+  addQueueFromLastElement(): string[] {
+    if (!(this.value instanceof Array))
+        return [];
+    let additionalQueue = this.value[this.value.length - 1].generateQueue();
+    this.queue = this.queue.concat(additionalQueue);
+    return additionalQueue;
+  }
+}
+
+class PieceBagBracket {
+  elements: PieceBagElement[] = [];
+  brac: string;
+  bracAt: number;
+
+  constructor(leftBracket: string, leftBracketAt: number) {
+    this.brac = leftBracket;
+    this.bracAt = leftBracketAt;
+  }
+
+  isMatched(c: string): boolean {
+    if (this.brac === "(" && c === ")") return true;
+    if (this.brac === "[" && c === "]") return true;
+    return false;
+  }
+}
+
+class PieceBag {
+  elementRoot: PieceBagElement;
+  doLoopLastElement: boolean = false;
+  nextCount: number;
+  
+  constructor(bagPattern: string, bagSet: BagSet, nextCount: number) {
+    this.nextCount = nextCount;
+
+    bagPattern = bagPattern.replaceAll("@", "IOTLJSZ");
+    let stack: PieceBagBracket[] = [
+      new PieceBagBracket("head", 0),
+    ];
+
+    for (let i = 0; i < bagPattern.length; i++) {
+      let c = bagPattern[i];
+
+      if (i === bagPattern.length - 1 && c === "~")
+        this.doLoopLastElement = true;
+      else if (c === "*") {
+        let repeatInfo = getRepeatCount(i);
+        i += repeatInfo.skipIndex;
+        for (let j = 0; j < repeatInfo.count; j++) {
+          stack[0].elements.push(stack[0].elements[stack[0].elements.length - 1].shallowCopy());
+        }
+      }
+      else if (c === "(" || c === "[") {
+        stack.unshift(new PieceBagBracket(c, i));
+      }
+      else if (stack[0].isMatched(c)) {
+        let e: PieceBagElement;
+        if (c === ")")
+          e = new PieceBagElement("grouped");
+        else if (c === "]")
+          e = new PieceBagElement("random-grouped", getTerminate(stack[0].bracAt, i, stack[0].elements.length));
+        else continue;
+
+        e.add(stack[0].elements);
+        stack[1].elements.push(e);
+        stack.shift();
+      }
+      else {
+        if (bagSet.includes(c))
+          stack[0].elements.push(new PieceBagElement("single", c));
+      }
+    }
+
+    this.elementRoot = new PieceBagElement("grouped");
+    this.elementRoot.add(stack[0].elements);
+
+    function getTerminate(beginIndex: number, endIndex: number, defaultv: number): number {
+      let s = bagPattern.slice(beginIndex + 1, endIndex).split(":");
+      if (s.length <= 1) return defaultv;
+      let result = Number.parseInt(s[1]);
+      if (isNaN(result)) return defaultv;
+      return result;
+    }
+
+    function getRepeatCount(beginIndex: number): {count: number, skipIndex: number} {
+      let s = bagPattern.slice(beginIndex + 1);
+      console.log(s);
+      let match = s.match(/^\d+/);
+      if (match) 
+        return {count: parseInt(match[0]) - 1, skipIndex: match[0].length};
+      return {count: 0, skipIndex: 0};
+    }
+  }
+
+  pickNext() {
+
+  }
+
+  getNexts() {
+
+  }
+
+
 }
 
 class KickTableSet {
@@ -157,7 +298,7 @@ class KickTable {
 }
 
 class Field {
-  
+
 }
 
 export {Shape, PieceBag, KickTable, KickTableSet};
--- src/components/v2/v2settings.ts
+++ src/components/v2/v2settings.ts
@@ -1,55 +1,27 @@
 type KickTableType = "SRS+";
 type shapeSetType = "standard";
-type bagType = "7bag" | "14bag";
 
 class GameSettings {
+  static readonly version: number = 1;
+
   fieldWidth: number = 10;
   fieldHeight: number = 20;
 
   kickTable: KickTableType = "SRS+";
   shapeSet: shapeSetType = "standard";
-  bag: bagType = "7bag";
+  nextCount: number = 5;
 
-  customBag: boolean = false;
-  customBagControl: string = "";
+  bagPattern: string = "";
   resetOnRunOutBag: boolean = true;
 
   enableUndo: boolean = false;
   enableQuickSave: boolean = false;
+  enableShowVirtualNext: boolean = false;
+  virtualNextBagCount: number = 0;
 
-  toJSON() {
-    return {
-      field: {
-        width: this.fieldWidth,
-        height: this.fieldHeight,
-      },
-      gameSetting: {
-        kickTable: this.kickTable,
-        shapeSet: this.shapeSet,
-        bag: this.bag,
-      },
-      functions: {
-        customBag: this.customBag,
-        customBagControl: this.customBagControl,
-        resetOnRunOutBag: this.resetOnRunOutBag,
-        enableUndo: this.enableUndo,
-        enableQuickSave: this.enableQuickSave,
-      },
-    }
-  }
-
-  static loadJSON(parsedData: any): GameSettings {
-    let result = new GameSettings();
-    result.fieldWidth = parsedData.field.width;
-    result.fieldHeight = parsedData.field.height;
-    result.kickTable = parsedData.gameSetting.kickTable;
-    result.shapeSet = parsedData.gameSetting.shapeSet;
-    result.bag = parsedData.gameSetting.bag;
-    result.customBag = parsedData.functions.customBag;
-    result.customBagControl = parsedData.functions.customBagControl;
-    result.resetOnRunOutBag = parsedData.functions.resetOnRunOutBag;
-    result.enableUndo = parsedData.functions.enableUndo;
-    result.enableQuickSave = parsedData.functions.enableQuickSave;
-    return result;
+  static loadJSON(json: string): GameSettings | undefined {
+    let obj = JSON.parse(json);
+    if (obj.version === GameSettings.version)
+      return Object.assign(new GameSettings(), json);
   }
 }
